8847a8edb2e3dc59a2fd3fee27660f7e984fcfa3
Yousif Faraj <yfaraj96@gmail.com>
2020-08-28 19:36:04 -0400

    fix parse syntax error

--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -1,5 +1,14 @@
 var Messages = {
-
+//big object that has a lot of objects
 
 };
 
+// var message = {
+//   username: this.username,
+//   text: this.text,
+//   roomname: this.room
+// };
+
+//if someone wants to see only messages in roomname or username
+//if we add all messages to messages
+//when we create a room we can have a default roomname
